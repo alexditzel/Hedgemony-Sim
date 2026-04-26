c488e3f1ad5cbc438fe5c22a4deec4e7540a26c8
Henry Blanchette <henry@rybl.net>
2026-04-26 15:27:03 -0400

    use iran-war scenario

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import defaultScenario from "./data/defaultScenario.json";
+// import scenario from "./data/defaultScenario.json";
+import scenario from "./data/iran-war.json";
 import { GameView } from "./components/GameView";
 import type { Scenario } from "./engine";
 
 export default function App() {
-  return <GameView scenario={defaultScenario as unknown as Scenario} />;
+  return <GameView scenario={scenario as unknown as Scenario} />;
 }
